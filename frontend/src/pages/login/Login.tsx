c881e37209eaaaf229a2fdfa5fadeb10e3cd0d63
Artem <artemfarafonov6@gmail.com>
2025-03-22 11:38:07 +0300

    feat(styles): added mixins, variables, updated entry styles

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import "./login.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUser } from "../../store/authSlice";
 import { AppDispatch, RootState } from "../../store/store";
 import { useNavigate } from "react-router-dom";
 import { Path } from "../../constants/Path";
+import { Link } from "react-router-dom";
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState("");
@@ -20,28 +22,40 @@ const Login: React.FC = () => {
     if (isLogined) {
       navigate(Path.home);
     }
-  },[isLogined]);
+  }, [isLogined]);
   return (
     <div className="login-container">
-      <h2>Вход</h2>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Пароль"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit" disabled={loading}>
-          {loading ? "Загрузка..." : "Войти"}
-        </button>
-        {error && <p className="error">{error}</p>}
-      </form>
+      <div className="login-card">
+        <h2>Вход</h2>
+        <form onSubmit={handleLogin}>
+          <div className="input-group">
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label>Пароль</label>
+            <input
+              type="password"
+              placeholder="Пароль"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button className="login-button" type="submit" disabled={loading}>
+            {loading ? "Загрузка..." : "Войти"}
+          </button>
+          {error && <p className="error">{error}</p>}
+        </form>
+        <p className="register-link">
+          Нет аккаунта? <Link to="/register">Зарегистрироваться</Link>
+        </p>
+      </div>
     </div>
   );
 };
